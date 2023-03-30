726ba76187120a680425f2db1deb0aeba31c0faf
Mael <laem@kont.me>
2023-03-30 19:41:57 +0200

    Télécharge tous les semgents

--- download.sh
+++ download.sh
@@ -3,4 +3,4 @@
 exec java -cp brouter.jar btools.server.RouteServer "segments4" "profiles2" "customprofiles" $PORT 1 0.0.0.0 &
 #wget -P segments4/ https://brouter.de/brouter/segments4/E0_N45.rd5
 #wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
-cat segments4/list.txt | xargs -n 1 -P 5 wget
+wget -i list.txt -P segments4
