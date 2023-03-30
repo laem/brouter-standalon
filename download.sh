ba8c5f221fa5dcbd01ecfaae22a5fca5b3166d71
Mael <laem@kont.me>
2023-03-30 18:45:25 +0200

    Luilui

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
-wget -O segments4 https://brouter.de/brouter/segments4/E0_N45.rd5
-wget -O segments4 https://brouter.de/brouter/segments4/W5_N45.rd5 
+#!/bin/bash
+
+wget -P segments4/ https://brouter.de/brouter/segments4/E0_N45.rd5
+wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
