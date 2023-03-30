a1f2976fd392f8557b7e750eea1bda4746b3648c
Mael <laem@kont.me>
2023-03-30 21:13:43 +0200

    Bretagne

--- download.sh
+++ download.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-exec java -cp brouter.jar btools.server.RouteServer "segments4" "profiles2" "customprofiles" $PORT 1 0.0.0.0 &
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N45.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N40.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E5_N40.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E5_N45.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N50.rd5
 wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
-wget -P segments4/ https://brouter.de/brouter/segments4/W5_N40.rd5 
+exec java -cp brouter.jar btools.server.RouteServer "segments4" "profiles2" "customprofiles" $PORT 1 0.0.0.0
 #wget -i list.txt -P segments4
