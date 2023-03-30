dbcbf55399a316f6228bf0927052c6e87eb055de
Mael <laem@kont.me>
2023-03-30 23:25:08 +0200

    Brest, je suis désolé

--- download.sh
+++ download.sh
@@ -8,5 +8,6 @@ wget -P segments4/ https://brouter.de/brouter/segments4/E5_N45.rd5
 wget -P segments4/ https://brouter.de/brouter/segments4/E0_N50.rd5
 wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
 wget -P segments4/ https://brouter.de/brouter/segments4/W5_N40.rd5
+wget -P segments4/ https://brouter.de/brouter/segments4/W10_N45.rd5 # Ouessant !
 exec java -cp brouter.jar btools.server.RouteServer "segments4" "profiles2" "customprofiles" $PORT 1 0.0.0.0
 #wget -i list.txt -P segments4
