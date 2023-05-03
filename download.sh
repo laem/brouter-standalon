89c45f16e7aa92b1fa2f0f3d0fbd2f18b41b06a8
Mael <laem@kont.me>
2023-05-03 21:22:11 +0200

    Retest avec le téchargement de tous les fichiers brouter

--- download.sh
+++ download.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N40.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N45.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E5_N40.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E5_N45.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/E0_N50.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
-wget -P segments4/ https://brouter.de/brouter/segments4/W5_N40.rd5
-wget -P segments4/ https://brouter.de/brouter/segments4/W10_N45.rd5 # Ouessant !
+#wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
+#wget -P segments4/ https://brouter.de/brouter/segments4/E0_N40.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/E0_N45.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/E5_N40.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/E5_N45.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/E0_N50.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/W5_N45.rd5 
+#wget -P segments4/ https://brouter.de/brouter/segments4/W5_N40.rd5
+#wget -P segments4/ https://brouter.de/brouter/segments4/W10_N45.rd5 # Ouessant !
+
+wget -i list.txt -P segments4
 exec java -cp brouter.jar btools.server.RouteServer "segments4" "profiles2" "customprofiles" $PORT 1 0.0.0.0
-#wget -i list.txt -P segments4
